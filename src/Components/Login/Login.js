1d4c30a0e0d2dbef12a7ca4aacfc04b6f65fe597
Mohsin Mahfooz <mernproject001@gmail.com>
2025-08-11 20:51:14 +0530

    Add files via upload

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div class="login">
+            <div id='login'>Login</div>
+            <input type='text' placeholder='Enter userName'>
+            </input>
+            <br></br>
+            <input type='password' placeholder='Enter password'>
+            </input>
+            <br></br>
+            <button>Login</button>
+        </div>
+    )
+}
+
+export default Login
