5c0f5d2fb319a5cca57d8b6e806fd9e7f64b295f
Mohsin Mahfooz <mernproject001@gmail.com>
2025-08-19 23:52:43 +0530

    page4.js

--- src/Pages/DbPages/page4.js
+++ src/Pages/DbPages/page4.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 // import './page4.css'
 
 const Page4 = () => {
     return (
         <div>
-            <div class="DbBanner">
+            <Link to='/dashboard' class="DbBanner">
                 <button class="DbBannerButton">⬅️ Back To DashBoard</button>
-            </div>
+            </Link>
 
             <span>2 Expiring 4-7 Days Member</span>
 
