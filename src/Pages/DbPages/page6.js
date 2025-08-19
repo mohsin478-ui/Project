59d59581d04c16219b68c0306be6250f97017738
Mohsin Mahfooz <mernproject001@gmail.com>
2025-08-19 23:54:33 +0530

    page6.js

--- src/Pages/DbPages/page6.js
+++ src/Pages/DbPages/page6.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 // import './page6.css'
 
 const Page6 = () => {
     return (
         <div>
-            <div class="DbBanner">
+            <Link to='/dashboard' class="DbBanner">
                 <button class="DbBannerButton">⬅️ Back To DashBoard</button>
-            </div>
+            </Link>
 
             <span>1 Inactive Members</span>
 
